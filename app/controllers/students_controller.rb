fa5fcce58c079a171b80371667598b4810e47b11
Arianna Kosten <arianna.kosten@gmail.com>
2019-09-06 22:47:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,7 +23,6 @@ class StudentsController < ApplicationController
     else
       @student.active = true
     end
-    @student.active
     @student.save
     redirect_to student_path(@student)
   end 
